3c9a6aaebe628357eb2123934cb2df1e09da2c6b
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2024-02-11 13:29:30 +0700

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,60 @@
+from main import menu_lang,custom_lang,clear_console
+import time,os,threading,platform
+
+def type_sender(meth,args):
+   a = '> NUL 2>&1'
+   if platform.system().lower() == 'linux':
+      a = '> /dev/null 2>&1'
+   os.system(f'python methods\\{meth}.py {args} %s'%(a))
+
+languages = menu_lang()
+menu = """               \x1b[38;5;196m╔═╗\x1b[38;5;197m═╗ ╦\x1b[38;5;198m╔═╗\x1b[38;5;40m╔╦╗\x1b[38;5;41m╔═╗\x1b[38;5;42m╔═╗\x1b[38;5;43m╦  \n               \x1b[38;5;196m╚═╗\x1b[38;5;197m╔╩╦╝\x1b[38;5;198m╠═╝ \x1b[38;5;76m║ \x1b[38;5;77m║ ║\x1b[38;5;78m║ ║\x1b[38;5;79m║  \n               \x1b[38;5;196m╚═╝\x1b[38;5;197m╩ ╚═\x1b[38;5;198m╩o  \x1b[38;5;112m╩\x1b[38;5;113m ╚═╝\x1b[38;5;114m╚═╝\x1b[38;5;115m╩═╝\n              \x1b[38;5;196m══╦══════════════════╦══\n    \x1b[38;5;197m╔═══════════╩══════════════════╩═══════════╗╗\n  \x1b[38;5;198m┏┓║         \x1b[38;5;76mWelcome \x1b[38;5;77mTo \x1b[38;5;208mS\x1b[38;5;209mX\x1b[38;5;210mP\x1b[38;5;211mv\x1b[38;5;212m2\x1b[38;5;255m.\x1b[38;5;226mDOS \x1b[38;5;78mTOOL        \x1b[38;5;198m║┏┳┓\n  \x1b[38;5;199m┃┃╚══════════════════════════════════════════╝┃┃┃\n  \x1b[38;5;200m┃┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛┃┃\n\x1b[38;5;201m┏━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻┻┳┓\n\x1b[38;5;200m┃\x1b[38;5;226m%s\x1b[38;5;200m┃┃\n\x1b[38;5;199m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻┛\n     \x1b[38;5;21mCopyright \x1b[38;5;57m© \x1b[38;5;93m%s \x1b[38;5;129mSXPv2 \x1b[38;5;165mAll \x1b[38;5;165mRight's \x1b[38;5;201mReserved\x1b[0m"""
+meth = """\x1b[38;5;197m╔════════════════════════════════╦════════════════════════╗\n\x1b[38;5;197m║ \x1b[38;5;76mHTTPS WEBSITE \x1b[38;5;255m[ \x1b[38;5;32mLINK \x1b[38;5;255m& \x1b[38;5;33mIP \x1b[38;5;255m]    \x1b[38;5;197m╠════════════════════════╣\n\x1b[38;5;197m╠════════════════════════════════╣ \x1b[38;5;76mCONNECTION \x1b[38;5;77mATTACK      \x1b[38;5;197m║\n\x1b[38;5;197m║ \x1b[38;5;76m╔═════════════╗                \x1b[38;5;197m║  \x1b[38;5;106mHANDSHAKE             \x1b[38;5;197m║\n\x1b[38;5;197m║ \x1b[38;5;77m║  \x1b[38;5;255mHANDSHAKE  \x1b[38;5;77m║                \x1b[38;5;197m║  \x1b[38;5;107mOVH-CONNECT           \x1b[38;5;197m║\n\x1b[38;5;197m║ \x1b[38;5;78m║   \x1b[38;5;255mBROWSER   \x1b[38;5;78m║                \x1b[38;5;197m║                        \x1b[38;5;197m║\n\x1b[38;5;197m║ \x1b[38;5;79m╚═════════════╝                \x1b[38;5;197m║ \x1b[38;5;161mHALF \x1b[38;5;161mBYPASS            \x1b[38;5;197m║\n\x1b[38;5;197m╠════════════════════════════════╣  \x1b[38;5;124mHTTP-QUERY\x1b[38;5;255m, \x1b[38;5;124mOVH-RPS   \x1b[38;5;197m║\n\x1b[38;5;197m║       \x1b[38;5;76mHTTP WEBSITE \x1b[38;5;255m[ \x1b[38;5;33mIP \x1b[38;5;255m]      \x1b[38;5;197m║  \x1b[38;5;124mBROWSER               \x1b[38;5;197m║\n\x1b[38;5;197m╠════════════════════════════════╣                        \x1b[38;5;197m║\n\x1b[38;5;197m║ \x1b[38;5;76m╔════════════╗ ╔═════════════╗ \x1b[38;5;197m║ \x1b[38;5;226mTOP METHODS USE BY DEV \x1b[38;5;197m║\n\x1b[38;5;197m║ \x1b[38;5;77m║ \x1b[38;5;255mHTTP-QUERY \x1b[38;5;77m║ ║   \x1b[38;5;255mOVH-RPS   \x1b[38;5;77m║ \x1b[38;5;197m║   \x1b[38;5;220mHTTP-QUERY           \x1b[38;5;197m║\n\x1b[38;5;197m║ \x1b[38;5;78m╚════════════╝ ║ \x1b[38;5;255mOVH-CONNECT \x1b[38;5;78m║ \x1b[38;5;197m║   \x1b[38;5;221mBROWSER              \x1b[38;5;197m║\n\x1b[38;5;197m║                \x1b[38;5;79m╚═════════════╝ \x1b[38;5;197m║   \x1b[38;5;222mHANDSHAKE            \x1b[38;5;197m║\n\x1b[38;5;197m║                                ╠════════════════════════╣\n\x1b[38;5;197m╚════════════════════════════════╩════════════════════════╝\x1b[0m"""
+atk = """                    \x1b[38;5;76m╔═╗═╗ ╦╔═╗  \x1b[38;5;226m┬  ┬┌─┐\n                    \x1b[38;5;77m╚═╗╔╩╦╝╠═╝  \x1b[38;5;227m└┐┌┘┌─┘\n                    \x1b[38;5;78m╚═╝╩ ╚═╩     \x1b[38;5;228m└┘ └─┘     \n           \x1b[38;5;76m█  ╚══╦═══════════════════════╦══╝  █\n           \x1b[38;5;77m╚══╦══╩═[   \x1b[38;5;196mSXPv2 \x1b[38;5;197mDOS\x1b[38;5;255m-\x1b[38;5;198mTOOL  \x1b[38;5;77m]═╩══╦══╝\n\x1b[38;5;78m╔═════════════╣\x1b[38;5;69m%s\x1b[38;5;78m╠═════════════╗\n   \x1b[38;5;160mTARGET \x1b[38;5;255m- \x1b[38;5;208m%s\n  \x1b[38;5;163mMETHODS \x1b[38;5;255m- \x1b[38;5;226m%s\n\x1b[38;5;78m╚═════════════╣       MADE BY HEX1629       ╠═════════════╝\n           \x1b[38;5;77m╔══╩══╦═[   \x1b[38;5;196mSXPv2 \x1b[38;5;197mDOS\x1b[38;5;255m-\x1b[38;5;198mTOOL  \x1b[38;5;77m]═╦══╩══╗\n           \x1b[38;5;76m█   ══╩═══════════════════════╩══   █\n                    \x1b[38;5;78m╔═╗╦ ╔═╦    \x1b[38;5;228m ┌┐ ┌─┐\n                    \x1b[38;5;77m╔═╝╚╦╩╗╠═╗ \x1b[38;5;227m ┌┘└┐└─┐\n                    \x1b[38;5;76m╚═╝═╝ ╩╚═╝  \x1b[38;5;226m┴  ┴└─┘\x1b[0m"""
+
+def format_banner(data):
+   return data.replace('\\x1b','\x1b').replace('\\n','\n')
+
+def controler():
+    global languages
+    print(menu%(languages['DISPLAY']['MAIN'],time.ctime().split( )[4]))
+    while True:
+     try:
+        commander = input("\x1b[38;5;76mS\x1b[38;5;77mX\x1b[38;5;78mP\x1b[38;5;255m.\x1b[38;5;226mT\x1b[38;5;227mO\x1b[38;5;228mO\x1b[38;5;229mL \x1b[38;5;196m--> \x1b[0m")
+        com = commander.split(' '); a = com[0].replace('!','').upper()
+        if a == 'HELP':
+           print(format_banner(languages['DISPLAY']['HELP']))
+        elif a == 'LANGUAGES':
+           c = 0
+           if len(com) == 2:
+              lang = com[1].upper()
+              if lang == 'TH' or lang == 'EN' or lang == 'AR' or lang == 'VN':
+                 languages = custom_lang(lang)
+                 c = 1
+              else:print(f'\x1b[38;5;196m Wrong Languages ( TH, VN, AR, EN ) !')
+           else:
+              languages = menu_lang()
+              c = 1
+           if c == 1:print(menu%(languages['DISPLAY']['MAIN'],time.ctime().split( )[4]))
+        elif a == 'CLS':
+           clear_console()
+        elif a == 'METH' or a == 'ATTACKS' or a == 'ATK' or a == 'HUB' or a == 'METHOD' or a == 'METHODS':
+           print(meth)
+        elif a == 'BROWSER' or a == 'المتصفح' or a.encode() == b'TR\xc3\x8cNHDUY\xe1\xbb\x86T' or a == 'เบราว์เซอร์':
+           if len(com) == 6:
+            target = com[1]
+            times = com[2]
+            thread = com[3]
+            meths = com[4]
+            p = com[5]
+            threading.Thread(target=type_sender,args=('BROWSER',f'{target} {times} {thread} {meths} {p} command.txt')).start()
+            print(atk%(languages['DISPLAY']['ATTACK'],target,a))
+           else:print(format_banner(languages['METHODS']['BROWSER']))
+        elif a == 'MENU':
+           clear_console()
+           print(menu%(languages['DISPLAY']['MAIN'],time.ctime().split( )[4]))
+        elif a == 'EXIT':
+           exit()
+        else:print(a.encode())
+     except KeyboardInterrupt:exit()
+controler()
---
+++ main.py
@@ -0,0 +1,98 @@
+import requests,json,sys,time,hashlib,platform,os
+
+def clear_console():
+    if platform.system().lower() == 'windows':
+       os.system('cls')
+    else:
+       os.system('clear')
+
+loader = {"Char1":"\\|/-"}
+
+def read(path):
+  try:
+   with open(path,'r') as f:return f.read()
+  except:
+    try:
+      with open(path,'rb') as f:return f.read()
+    except:return False
+
+def write(path,contents):
+  try:
+   with open(path,'w') as f:f.write(contents)
+  except:
+    with open(path,'wb') as f:f.write(contents)
+
+def setup_lang():
+  try:
+    r = requests.get('https://raw.githubusercontent.com/Tool-Free/socketexplodev2_assets/main/lang.json').content.decode()
+    return r
+  except:return False
+
+def custom_lang(lang):
+  json_lang = json.loads(setup_lang())
+  c = json_lang[lang]
+  for a in loader['Char1']:
+    sys.stdout.write(f"\r\x1b[0;m{c['LOG']['LANG']%(a)} [{lang}]\033[K")
+    sys.stdout.flush()
+    time.sleep(0.5)
+  print(f"\r\x1b[0;m{c['LOG']['FINISH']} . . .\033[K")
+  clear_console()
+  return c
+def menu_lang():
+    json_loads = json.loads(read('setting.json')); json_lang = json.loads(setup_lang())
+    lang = json_loads['MENU']['LANGUAGES']
+    c = json_lang[lang]
+    for a in loader['Char1']:
+     sys.stdout.write(f"\r\x1b[0;m{c['LOG']['LANG']%(a)} [{lang}]\033[K")
+     sys.stdout.flush()
+     time.sleep(0.5)
+    print(f"\r\x1b[0;m{c['LOG']['FINISH']} . . .\033[K")
+    clear_console()
+    return c
+
+def access_file(link,mode='a'):
+ try:
+   r = requests.get(link).content.decode()
+   if mode == 'r':r = r.replace('\n','').replace('\r','')
+   return r
+ except:return False
+
+def hash_checked(data,data2):
+  update_data = hashlib.sha256(data.encode()).hexdigest()
+  if data2 != False:
+   current_data = hashlib.sha256(data2.encode()).hexdigest()
+   if current_data == update_data:return True
+  return False
+
+def update_checked():
+  languages = menu_lang()
+  content = access_file('https://raw.githubusercontent.com/Hex1629/SOCKETEXPLODEv2_DOSTOOL/main/status.txt','r')
+  if content != False:
+    if content == 'Default':
+      c = access_file(f'https://raw.githubusercontent.com/Tool-Free/socketexplodev2_assets/main/update.json')
+      if c != False:
+        json_down = json.loads(c)
+        for a in json_down['FILE'].keys():
+          contents = access_file(f"{json_down['FILE'][a]}")
+          if contents != False:
+             if hash_checked(contents,read(a)) == False:
+              write(a,contents); print(languages['CHECK_MSG']['UPDATE']%a)
+        print(languages['CHECK_MSG']['DEFAULT'])
+        os.system('python gui.py')
+    elif content == 'Shutdown':
+      print(languages['CHECK_MSG']['SHUTDOWN']); exit()
+    else:
+      content = content.split(' ')
+      if content[0] == 'Update':
+        print(languages['CHECK_MSG']['REPORT'])
+        c = access_file(f'https://raw.githubusercontent.com/Hex1629/SOCKETEXPLODEv2_DOSTOOL/main/{content[1]}')
+        if c != False:
+          json_down = json.loads(c)
+          for a in json_down['FILE'].keys():
+            contents = access_file(f"{json_down['FILE'][a]}")
+            if contents != False:
+             if hash_checked(contents,read(a)) == False:
+              write(a,contents); print(languages['CHECK_MSG']['UPDATE']%a)
+             else:print(languages['LOG']['SAME']%a)
+          print(languages['LOG']['DONE'])
+          os.system('python gui.py')
