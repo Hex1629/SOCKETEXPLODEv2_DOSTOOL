29327361183f348d78607be152f90a1120a50811
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2024-03-12 22:46:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json,time
 from gui import controler
 from attrs import access_file,menu_lang,hash_checked,read,write,color_gardient,green_gr,yellow_gr,red_gr,blue_gr
-
+mode = 0
 def update_checked():
   languages = menu_lang()
   content = access_file('https://raw.githubusercontent.com/Hex1629/SOCKETEXPLODEv2_DOSTOOL/main/status.txt','r')
@@ -15,9 +15,14 @@ def update_checked():
           if contents != False:
              if hash_checked(contents,read(a)) == False:
               write(a,contents); print(color_gardient(languages['CHECK_MSG']['UPDATE']%a,yellow_gr))
+              mode = 1
         time.sleep(1)
         print(color_gardient(languages['CHECK_MSG']['DEFAULT'],green_gr))
-        controler()
+        if mode == 0:controler()
+        else:
+          print(color_gardient('RESTART PROGRAM'%a,yellow_gr))
+          with open('main.py','r') as f:
+            exec(f.read())
     elif content.decode() == 'Shutdown':
       print(color_gardient(languages['CHECK_MSG']['SHUTDOWN'],red_gr)); exit()
     else:
@@ -32,7 +37,12 @@ def update_checked():
             if contents != False:
              if hash_checked(contents,read(a)) == False:
               write(a,contents); print(languages['CHECK_MSG']['UPDATE']%a)
+              mode = 1
              else:print(color_gardient(languages['LOG']['SAME']%a,blue_gr))
           print(languages['LOG']['DONE'])
-          controler()
+          if mode == 0:controler()
+          else:
+           print(color_gardient('RESTART PROGRAM'%a,yellow_gr))
+           with open('main.py','r') as f:
+            exec(f.read())
 update_checked()
