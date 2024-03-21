a972e2a256cab3208ca2ad2c378f8b90b4f7ca13
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2024-03-22 02:18:53 +0700

    Update banner.py

--- banner.py
+++ banner.py
@@ -1,4 +1,12 @@
-import json,requests,re
+import requests,json,re, math
+
+millnames = ['','k','M','B','T']
+
+def millify(n):
+    n = float(n)
+    millidx = max(0, min(len(millnames)-1, int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+    return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
+
 
 def query(lang,meth):
    r = json.loads(requests.get('https://raw.githubusercontent.com/Tool-Free/socketexplodev2_assets/main/sys.json').content)
@@ -25,37 +33,94 @@ def json_error_check(r):
     if c != 0:exit_json.append(a)
  return r.replace(''.join(exit_json), ''.join(exit_json).replace(',', '')) if c != 0 else False
 
+def get_review():
+    try:
+     r = requests.get('http://127.0.0.1:5000/REVIEW').json()
+     with open('Cache.txt','w') as f:
+        json.dump(r,f)
+    except:
+       try:
+         with open('Cache.txt','r') as f:r = json.loads(f.read())
+       except:r= 'CANT CONN!'
+    return r
+
+def get_rank():
+    try:
+     r = requests.get('http://127.0.0.1:5000/RANKALL').json()
+     with open('Cache_rank.txt','w') as f:
+        json.dump(r,f)
+    except:
+       try:
+         with open('Cache_rank.txt','r') as f:r = json.loads(f.read())
+       except:r= 'CANT CONN!'
+    return r
+
 def methods():
    z,r,a = '','',''
    z = requests.get('https://raw.githubusercontent.com/Tool-Free/socketexplodev2_assets/main/sys.json').content
    a = json_error_check(z.decode())
    if a == False:r = json.loads(z)
    else:r = json.loads(a.encode())
-   meth = ["HTTP-19","HANDSHAKE","HANDSHAKE2","RAPID-FAST","BROWSER","AMP","MURD-OPT","MURD","MURD-PROXY","TCP-RST","UDP-STORM","UDP-SLOW","UDP-DOUBLE"]
-   ok_color,layer_text,text_description1 = '\x1b[38;5;82m','\x1b[48;5;70m\x1b[38;5;255m','\x1b[38;5;82m'
-   no_color,layer_text2,text_description2 = '\x1b[38;5;196m','\x1b[48;5;160m\x1b[38;5;255m','\x1b[38;5;197m'
-   p = '\x1b[38;5;196m╔═╗\x1b[38;5;202m─┐ ┬\x1b[38;5;208m┌─┐\x1b[38;5;76m╦  ╦╔═╗\n\x1b[38;5;197m╚═╗\x1b[38;5;203m┌┴┬┘\x1b[38;5;209m├─┘\x1b[38;5;77m╚╗╔╝╔═╝\n\x1b[38;5;198m╚═╝\x1b[38;5;205m┴ └─\x1b[38;5;210m┴  \x1b[38;5;78m ╚╝ ╚═╝ \x1b[38;5;226mHUB                                   \x1b[38;5;87mMADE \x1b[38;5;86mBY \x1b[38;5;85mHEX1629\n\x1b[38;5;75m╔══════════════════════════════════════════════════════════════════════\n\x1b[38;5;75m║ \x1b[38;5;226mMethods       \x1b[38;5;227mLayer  \x1b[38;5;228mDescription\n'
-   panel = '\x1b[38;5;75m╠═════════════╦══════╦═════════════════════════════════════════════════\n\x1b[38;5;75m║ {}HTTP-19     \x1b[38;5;75m║ {} L7 \x1b[0m \x1b[38;5;75m║ {}Optimize for High RP/S.\n\x1b[38;5;75m╠═════════════╬══════╬═════════════════════════════════════════════════\n\x1b[38;5;75m║ {}HANDSHAKE   \x1b[38;5;75m║ {} L7 \x1b[0m \x1b[38;5;75m║ {}Send packet for flooding and reset after send.\n\x1b[38;5;75m║ {}HANDSHAKE2  \x1b[38;5;75m║ {} L7 \x1b[0m \x1b[38;5;75m║ {}Re-create socket for flooding auto.\n\x1b[38;5;75m║ {}RAPID-FAST  \x1b[38;5;75m║ {} L7 \x1b[0m \x1b[38;5;75m║ {}Optimize for HIGH RP/S and keep connection.\n\x1b[38;5;75m║ {}BROWSER     \x1b[38;5;75m║ {} L7 \x1b[0m \x1b[38;5;75m║ {}Flooding HTTP/S with like packet from browser.\n\x1b[38;5;75m║ {}AMP         \x1b[38;5;75m║ {} L7 \x1b[0m \x1b[38;5;75m║ {}Kill website with HIGH mbp/s.\n\x1b[38;5;75m║ {}MURD-OPT    \x1b[38;5;75m║ {} L7 \x1b[0m \x1b[38;5;75m║ {}Down website with keep connection.\n\x1b[38;5;75m║ {}MURD        \x1b[38;5;75m║ {} L7 \x1b[0m \x1b[38;5;75m║ {}MURD-OPT but Optimize for like browser more.\n\x1b[38;5;75m║ {}MURD-PROXY  \x1b[38;5;75m║ {} L7 \x1b[0m \x1b[38;5;75m║ {}MURD but use all proxy type.\n\x1b[38;5;75m╠═════════════╬══════╬═════════════════════════════════════════════════\n\x1b[38;5;75m║ {}TCP-RST     \x1b[38;5;75m║ {} L4 \x1b[0m \x1b[38;5;75m║ {}Flooding TCP with HIGH mbp/s.\n\x1b[38;5;75m║ {}UDP-STORM   \x1b[38;5;75m║ {} L4 \x1b[0m \x1b[38;5;75m║ {}Flooding UDP with High Mbp/s for flooding layer4\n\x1b[38;5;75m║ {}UDP-SLOW    \x1b[38;5;75m║ {} L4 \x1b[0m \x1b[38;5;75m║ {}Long time for flooding target then UDP-STORM.\n\x1b[38;5;75m║ {}UDP-DOUBLE  \x1b[38;5;75m║ {} L4 \x1b[0m \x1b[38;5;75m║ {}Flooding UDP with content page.\n\x1b[38;5;75m╚═════════════╩══════╩═════════════════════════════════════════════════'
-   panel_b = []
-   c = 0
-   for b in panel.split('\n'):
-    c += 1
-    if '╔═' in b or '╚═' in b or '╠═' in b:
-      e = '\n'
-      if c == len(panel.split('\n')):e = ''
-      panel_b.append(b+e)
-    else:
-        regex = r"(\w+[A-Z].\w+)\s*(.*)"
-        matches = re.findall(regex, b)
-        for match in matches:
-            c2 = 0
-            if match[0] in meth:
-                try:
-                    meth_maintenance = r['FIX'][match[0]]
-                    if meth_maintenance.lower() == 'yes' or meth_maintenance.upper() == 'TRUE':panel_b.append((b.format(no_color,layer_text2, text_description2)+'\x1b[0m\n'))
-                except:
-                    c2 = 1
-            if c2 == 1:
-                panel_b.append((b.format(ok_color,layer_text, text_description1)+'\x1b[0m\n'))
-   panel_b.append('\n\x1b[38;5;199m(\x1b[38;5;196mRED\x1b[38;5;199m) \x1b[38;5;198mcurrent maintenance \x1b[38;5;79m(\x1b[38;5;70mGREEN\x1b[38;5;79m) \x1b[38;5;72mcurrent normal\x1b[0m')
-   return p+''.join(panel_b)
+   data_table = [(' HTTP-19    ', '  L7   ', ' TCP  ', ' Attack http with RP/S.               '), (' HANDSHAKE  ', '  L7   ', ' TCP  ', ' Trying create socket for flooding.   '), (' HANDSHAKE2 ', '  L7   ', ' TCP  ', ' Create socket auto after have error. '), (' RAPID-FAST ', '  L7   ', ' TCP  ', ' Less line for flooding https.        '), (' BROWSER    ', '  L7   ', ' TCP  ', ' Have header from browser.            '), (' MURD-OPT   ', '  L7   ', ' TCP  ', ' Make website overload with connect.  '), (' MURD       ', '  L7   ', ' TCP  ', ' MURD-OPT but like browser more.      '), (' MURD-PROXY ', '  L7   ', ' TCP  ', ' IT MURD but have proxy.              '), (' LOIC       ', '  L7   ', ' TCP  ', ' Requests flooding not socket.        '), (' TCP-RST    ', '  L4   ', ' TCP  ', ' High Mbp/s for flooding.             '), (' UDP-STORM  ', '  L4   ', ' UDP  ', ' High Mbp/s but short time.           '), (' UDP-SLOW   ', '  L4   ', ' UDP  ', ' Stable flooding.                     '), (' UDP-DOUBLE ', '  L4   ', ' UDP  ', ' Flooding with content http.          ')]
+   table_data = '   %s   \x1b[38;5;255m║%s\x1b[38;5;255m║%s\x1b[38;5;255m║%s\x1b[38;5;255m║%s\x1b[38;5;255m║%s\x1b[38;5;255m║%s\x1b[0m'
+   data = get_review()
+   data2 = get_rank()
+   ranked_methods = [('00', 'FAILED', '0', '0')] * 13
+   if data2 != 'CANT CONN!':
+    ranked_methods = []
+    for rank, (name, values) in enumerate(data2.items(), start=1):ranked_methods.append((str('{:02d}'.format(rank)), name, values['GOOD'],values['FAIL']))
+   banner = ['\n \x1b[38;5;57mSTATUS     \x1b[38;5;63mPOWER     \x1b[38;5;69mNAME         \x1b[38;5;75mLAYER   \x1b[38;5;81mTYPE   \x1b[38;5;87mDESCRIPTION','\x1b[38;5;255m════════╦═══════════╦════════════╦═══════╦══════╦══════════════════════════════════════╦ \x1b[38;5;78mTOP \x1b[38;5;79mNAME       \x1b[38;5;80mGOOD  \x1b[38;5;81mFAIL\x1b[0m']
+   fix,text = '\x1b[38;5;197m✗','\x1b[38;5;197m'
+   normal,text2 = '\x1b[38;5;70m🗸','\x1b[38;5;70m'
+   append_line = '\x1b[38;5;76m %s  \x1b[38;5;77m%s \x1b[38;5;78m%s  \x1b[38;5;79m%s'
+   count = 0
+   for a in data_table:
+      b=''
+      if data != 'CANT CONN!':
+        appned_b = []
+        for b in data[a[0].replace(' ','')]:appned_b.append(f'{b} ')
+      com = ()
+      c2 = 0
+      try:
+        meth_maintenance = r['FIX'][a[0].replace(' ','')]
+        if meth_maintenance.lower() == 'yes' or meth_maintenance.upper() == 'TRUE':com=(fix,text)
+      except:c2 = 1
+      if c2 == 1:com=(normal,text2)
+      color_description = ''
+      if data != 'CANT CONN!':
+        if len(appned_b) == 0:color_description = '\x1b[38;5;255m';b = '           '
+        else:
+         b = '\x1b[38;5;200m'
+         raw = []
+         raw2 = []
+         b_c = 0
+         count_e = 200
+         for t_x in appned_b:
+           count_e -= 1
+           raw.append(f'\x1b[38;5;{count_e}m'+t_x)
+           raw2.append(t_x)
+           b_c += 1
+           if b_c == len(appned_b):
+             color_description = f'\x1b[38;5;{count_e}m'
+        
+         b = ' '.join(raw)
+         b2 = ' '.join(raw2)
+         b += ' '*int(len('           ')-len(b2))
+      else:b = '           '
+      d = ranked_methods[count]
+      t,t2 = '',''
+      if len(d[2]) > 1000:t = d[2]+' '*(4-len(d[2]))
+      else:
+        m = millify(int(d[2]))
+        t = m+' '*(4-len(m))
+      if len(d[3]) > 1000:t2 = d[3]+' '*(4-len(d[3]))
+      else:
+         m = millify(int(d[3]))
+         t2 = m+' '*(4-len(m))
+      r2 = (t,t2)
+      banner.append(table_data%(com[0],b,com[1]+a[0],'\x1b[48;5;226m\x1b[38;5;255m'+a[1]+'\x1b[0m','\x1b[48;5;208m\x1b[38;5;255m'+a[2]+'\x1b[0m',color_description+a[3],append_line%(d[0],d[1]+' '*(10-len(d[1])),r2[0],r2[1])))
+      count += 1
+   banner.append('\x1b[38;5;255m════════╩═══════════╩════════════╩═══════╩══════╩══════════════════════════════════════╩ \x1b[38;5;78mTOP \x1b[38;5;79mNAME       \x1b[38;5;80mGOOD  \x1b[38;5;81mFAIL\x1b[0m')
+   banner.append('                   \x1b[38;5;255m( \x1b[38;5;70m🗸 \x1b[38;5;71mCURRENT NORMAL \x1b[38;5;196m✗ \x1b[38;5;197mCURRENT MAINTENENCE \x1b[38;5;255m)\n\x1b[38;5;226m♔ \x1b[38;5;227mMORE IS BETTER')
+   banner.append('\n\x1b[38;5;51mMADE \x1b[38;5;50mBY \x1b[38;5;202mt\x1b[38;5;203m.\x1b[38;5;204mm\x1b[38;5;205me\x1b[38;5;206m/\x1b[38;5;207mjack2044 \x1b[38;5;49mand \x1b[38;5;202mt\x1b[38;5;203m.\x1b[38;5;204mm\x1b[38;5;205me\x1b[38;5;206m/\x1b[38;5;207mIDKOTHERHEX1629')
+   return '\n'.join(banner)+'\x1b[0m'
