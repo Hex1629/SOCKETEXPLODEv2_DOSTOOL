b26786e66bbf20808e49e6f9e09089d2e38c1247
DeAngelo-png <148593933+DeAngelo-png@users.noreply.github.com>
2024-04-13 13:43:00 -0700

    Update main.py
    Added some documentation, improved readability, and cleaned up some unnecessary variables, simplified the code and optimized by assigning values instead of if statements.

--- main.py
+++ main.py
@@ -1,88 +1,106 @@
-import json,time,threading,sys
-from gui import controler
-from attrs import access_file,menu_lang,hash_checked,read,write,color_gardient,green_gr,yellow_gr,red_gr,blue_gr,update_pypi
-languages = menu_lang()
+import requests
+import json
+import re
+import math
 
-def is_update(file='update.json'):
-    c = access_file(f'https://raw.githubusercontent.com/Tool-Free/socketexplodev2_assets/main/{file}')
-    if c != False:
-        c = c.replace(b',\n  }\n}', b'\n  }\n}')
-        json_down = json.loads(c)
-        r3 = json_down['PIP']
-        c2 = 0
-        for b in r3.keys():
-           c2 += 1; sys.stdout.write(f"\r\x1b[38;5;255m[\x1b[38;5;75mINFO\x1b[38;5;255m] \x1b[38;5;198m{b} \x1b[38;5;255mof \x1b[38;5;197m{c2}\x1b[38;5;255m/\x1b[38;5;196m{len(r3.keys())}\x1b[0m\x1b[K"); sys.stdout.flush()
-           time.sleep(0.5)
-           craft_payload = f"""import os, sys, time
-load = ['','.','. .','. . .','. . . .']
-try:
-    {r3[b]['import']}
-    print("\\r\\x1b[K\\x1b[38;5;196mDONE \\x1b[38;5;197mINSTALL \\x1b[38;5;198m{b} \\x1b[38;5;199mPIP!\\x1b[0m")
-except:
-    os.system("{r3[b]['command']}")
-    for b2 in load:
-        sys.stdout.write(f"\\r\\x1b[38;5;255m[\\x1b[38;5;76mWARN\\x1b[38;5;255m] \\x1b[38;5;198m{b} \\x1b[38;5;197m"""+"""{b2}"""+"""\\x1b[0m\\x1b[K")
-        sys.stdout.flush() 
-        time.sleep(0.1)
-    print(f'\\r\\x1b[K\\x1b[38;5;255m[\\x1b[38;5;196mDONE\\x1b[38;5;255m] \\x1b[38;5;226m{b} \\x1b[38;5;227m. . . . \\x1b[38;5;228mdone\\x1b[0m')"""
-           exec(craft_payload)
-           time.sleep(0.5)
-        load = ['','.','. .','. . .','. . . .']
-        c2 = 0
-        print(f'\r\x1b[38;5;255m[\x1b[38;5;76mD\x1b[38;5;77mO\x1b[38;5;78mWN\x1b[38;5;79mL\x1b[38;5;80mO\x1b[38;5;81mA\x1b[38;5;117mD\x1b[38;5;255m] \x1b[38;5;196m{file}\x1b[0m\x1b[K')
-        for a in json_down['FILE'].keys():
-          contents = access_file(f"{json_down['FILE'][a]}",'github')
-          if contents != False:
-             c2 += 1
-             time.sleep(0.1)
-             sys.stdout.write(f"\r\x1b[38;5;255m[\x1b[38;5;75mINFO\x1b[38;5;255m] \x1b[38;5;198m{a} \x1b[38;5;255mof \x1b[38;5;197m{c2}\x1b[38;5;255m/\x1b[38;5;196m{len(json_down['FILE'].keys())}\x1b[0m\x1b[K")
-             sys.stdout.flush()
-             time.sleep(0.5)
-             for b in load:
-              sys.stdout.write(f"\r\x1b[38;5;255m[\x1b[38;5;76mWARN\x1b[38;5;255m] \x1b[38;5;198m{a} \x1b[38;5;197m{b}\x1b[0m\x1b[K")
-              sys.stdout.flush()
-             time.sleep(0.1)
-             if hash_checked(contents,read(a)) == False:threading.Thread(target=write,args=(a,contents)).start(); print(f'\r\x1b[K\x1b[38;5;255m[\x1b[38;5;196mDONE\x1b[38;5;255m] \x1b[38;5;226m{a} \x1b[38;5;227m. . . . \x1b[38;5;228mdownload\x1b[0m')
-             else:print(f'\r\x1b[K\x1b[38;5;255m[\x1b[38;5;75mDONE\x1b[38;5;255m] \x1b[38;5;76m{a} \x1b[38;5;77m. . . . \x1b[38;5;78mdefault\x1b[0m')
-with open('setting.json','r') as f:
-   a = json.loads(f.read())
-   if int(a['MENU']['TOU']) == 0:
-     content = ['''\x1b[1m\x1b[38;5;196mTerms of use\x1b[0m\n\n\x1b[38;5;197mNo support anything for down server game.\nThere is nobody say "\x1b[4mhow to use that methods\x1b[0m\x1b[38;5;197m" bc i write it.\nCan hitting dstat if you own or you trust.\nHitting layer7 target on layer4 it allow if can down.\nAnd if cant down some website it mean you wifi it weak or etc.\x1b[0m''','''\x1b[1m\x1b[38;5;196mPersonal information and security.\x1b[0m\n\n\x1b[38;5;197mThere it not have logs but if have it only for check country and etc.\nIf i leak you information like "\x1b[4mIP\x1b[0m\x1b[38;5;197m", "\x1b[4mCOUNTRY\x1b[0m\x1b[38;5;197m", "\x1b[4mCITY\x1b[0m\x1b[38;5;197m" it for what? i not want to doxing my client.\nThis log will be coming soon it only for check languages and most country use will be make dev for develop anything good,\nfor sure nobody like logs but it not like keylogger.\nSince if you trust dev and if you still not trust i will encrypt with AES-256 and i not keep key in my server it mean that key will be got remove forever.\nLast message i can sent anything file to all ppl use my tool.\nBut i not did bc it my client not you client.\x1b[0m''','''\x1b[1m\x1b[38;5;196mSupport tool.\x1b[0m\n\n\x1b[38;5;197mMy tool it support all like SXP, BOTc2, URANIUM since i was want to support. and check more proof at \x1b[3m\x1b[4mhttps://t.me/+gcA7xQYlsK83ZmRl.\x1b[0m\x1b[38;5;197m\nit proof group but i was boradcast any news to you like "\x1b[4mupdate or more\x1b[0m\x1b[38;5;197m".\nSince you can report any bug in group but github i was resp like 2 week or month bc i was know now but lazy.\x1b[0m''','''\x1b[5;3;1m\x1b[38;5;226mNow you can enter for running tool.\x1b[0m''']
-     CONTENT = 0
-     import platform
-     if platform.system().upper() == 'WINDOWS':
-      print("PRESS RIGHT ARROW FOR READ CONTENT.")
-      try:
-       import msvcrt
-       while True:
-        key = msvcrt.getch()
-        if key == b'\r':
-         if CONTENT == 4:
+millnames = ['', 'k', 'M', 'B', 'T']
+
+
+def millify(n): # format large numbers
+    n = float(n)
+    millidx = max(0, min(len(millnames)-1, int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+    return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
+
+def query(lang, meth):# function to query data from a JSON API
+    r = json.loads(requests.get('https://raw.githubusercontent.com/Tool-Free/socketexplodev2_assets/main/sys.json').content)
+    try:
+        a = r['FIX'][meth]
+        return (True, r['LANG'][lang])
+    except:
+        return (False, 'NULL')
+
+def json_error_check(r): # function to check and clean json data for errors
+    count = 0
+    error_append = []
+    for a in r.split('\n'):
+        if count == 1:
+            error_append.append(a)
+        count = 1 if a.replace(' ', '').encode() == b'"FIX":{' else count
+        try:
+            error_append.remove('  },')
             break
-        elif key == b'\xe0':
-         key = msvcrt.getch()
-         if key == b'K': 
-            if CONTENT > 0:CONTENT -= 1
-         elif key == b'M':
-            if CONTENT < 4:CONTENT += 1
-        if CONTENT != 0:print('\x1b[2J\x1b[H'+content[CONTENT-1])
-       a['MENU']['TOU'] = "1"
-       with open('setting.json','w') as f:json.dump(a,f)
-      except:pass
-     else:print("Sad You Cant Read TERMS OF USE!")
-update_pypi()
-content = access_file('https://raw.githubusercontent.com/Hex1629/SOCKETEXPLODEv2_DOSTOOL/main/status.txt','r')
-if content != False:
-    if content.decode() == 'Default':
-       is_update()
-       print(color_gardient(languages['CHECK_MSG']['DEFAULT'],green_gr))
-       controler()
-    elif content.decode() == 'Shutdown':
-      print(color_gardient(languages['CHECK_MSG']['SHUTDOWN'],red_gr)); exit()
-    else:
-      content = content.decode().split(' ')
-      if content[0] == 'Update':
-         print(languages['CHECK_MSG']['REPORT'])
-         is_update(content[1])
-         print(languages['LOG']['DONE'])
-         controler()
+        except:
+            pass
+    c, pattern = 0, r':(\".*)'
+    exit_json = []
+    for a in error_append:
+        matches = re.findall(pattern, a.replace(' ', ''))
+        for match in matches:
+            if '"YES",' in match.upper() or '"TRUE",' in match.upper() or '"NULL",':
+                c += 1
+            elif '"YES"' in match.upper() or '"TRUE"' in match.upper() or '"NULL"':
+                c = 0
+        if c != 0:
+            exit_json.append(a)
+    return r.replace(''.join(exit_json), ''.join(exit_json).replace(',', '')) if c != 0 else False
+
+def get_data(url, filename): # fetch data from an API or cache
+    try:
+        r = requests.get(url).json()
+        with open(filename, 'w') as f:
+            json.dump(r, f)
+    except:
+        try:
+            with open(filename, 'r') as f:
+                r = json.loads(f.read())
+        except:
+            r = 'CANT CONN!'
+    return r
+
+def get_review(): # fetch review data
+    return get_data('https://b95e6e1b-d19e-43e6-a070-92d5a04bceaf-00-6igc6jzjmncr.spock.replit.dev/REVIEW', 'Cache.txt')
+
+def get_rank(): # fetch ranking data
+    return get_data('https://b95e6e1b-d19e-43e6-a070-92d5a04bceaf-00-6igc6jzjmncr.spock.replit.dev/RANKALL', 'Cache_rank.txt')
+
+def methods(): # process stuff
+    z = requests.get('https://raw.githubusercontent.com/Tool-Free/socketexplodev2_assets/main/sys.json').content
+    a = json_error_check(z.decode())
+    r = json.loads(a.encode()) if a != False else json.loads(z)
+    data_table = [(' HTTP-19    ', '  L7   ', ' TCP  ', ' Attack http with RP/S.               '), (' HANDSHAKE  ', '  L7   ', ' TCP  ', ' Trying create socket for flooding.   '), (' HANDSHAKE2 ', '  L7   ', ' TCP  ', ' Create socket auto after have error. '), (' CONNECT    ', '  L7   ', ' TCP  ', ' It only connect method.              '), (' COOKIE     ', '  L7   ', ' TCP  ', ' Random cookie for flooding.          '), (' RAPID-FAST ', '  L7   ', ' TCP  ', ' Less line for flooding https.        '), (' BROWSER    ', '  L7   ', ' TCP  ', ' Have header from browser.            '), (' HYBRID     ', '  L7   ', ' TCP  ', ' H2 + H1.1 Flooding.                  '), (' MURD-OPT   ', '  L7   ', ' TCP  ', ' Make website overload with connect.  '), (' MURD       ', '  L7   ', ' TCP  ', ' MURD-OPT but like browser more.      '), (' MURD-PROXY ', '  L7   ', ' TCP  ', ' IT MURD but have proxy.              '), (' LOIC       ', '  L7   ', ' TCP  ', ' Requests flooding not socket.        '), (' TCP-RST    ', '  L4   ', ' TCP  ', ' High Mbp/s for flooding.             '), (' UDP-STORM  ', '  L4   ', ' UDP  ', ' High Mbp/s but short time.           '), (' UDP-SLOW   ', '  L4   ', ' UDP  ', ' Stable flooding.                     '), (' UDP-DOUBLE ', '  L4   ', ' UDP  ', ' Flooding with content http.          ')]
+    data = get_review()
+    data2 = get_rank()
+    ranked_methods = [('00', 'FAILED', '0', '0')] * 13
+    if data2 != 'CANT CONN!':
+        ranked_methods = [(str('{:02d}'.format(rank)), name, millify(int(values['GOOD'])), millify(int(values['FAIL']))) for rank, (name, values) in enumerate(data2.items(), start=1)]
+    banner = ['\n \x1b[38;5;57mSTATUS     \x1b[38;5;63mPOWER     \x1b[38;5;69mNAME         \x1b[38;5;75mLAYER   \x1b[38;5;81mTYPE   \x1b[38;5;87mDESCRIPTION','\x1b[38;5;255m════════╦═══════════╦════════════╦═══════╦══════╦══════════════════════════════════════╦ \x1b[38;5;78mTOP \x1b[38;5;79mNAME       \x1b[38;5;80mGOOD  \x1b[38;5;81mFAIL\x1b[0m']
+    fix, text = '\x1b[38;5;197m✗ ', '\x1b[38;5;197m'
+    normal, text2 = '\x1b[38;5;70m🗸 ', '\x1b[38;5;70m'
+    append_line = '\x1b[38;5;76m %s  \x1b[38;5;77m%s \x1b[38;5;78m%s  \x1b[38;5;79m%s'
+    count = 0
+    for a in data_table:
+        b = ''
+        if data != 'CANT CONN!':
+            appned_b = [f'{b} ' for b in data[a[0].replace(' ', '')]]
+            if len(appned_b) == 0:
+                color_description = '\x1b[38;5;255m'
+                b = ' ' * len('           ')
+            else:
+                b = '\x1b[38;5;200m' + ' '.join(appned_b)
+                b2 = ' '.join(appned_b)
+                b += ' ' * (len('           ') - len(b2))
+                color_description = f'\x1b[38;5;{200 - len(appned_b)}m'
+        else:
+            b = '           '
+            color_description = ''
+        d = ranked_methods[count]
+        r2 = (d[2], d[3])
+        com = (fix, text) if r['FIX'].get(a[0].replace(' ', ''), '').lower() in ['yes', 'true'] else (normal, text2)
+        banner.append(table_data % (com[0], b, com[1] + a[0], '\x1b[48;5;172m\x1b[38;5;232m' + a[1] + '\x1b[0m', '\x1b[48;5;208m\x1b[38;5;255m' + a[2] + '\x1b[0m', color_description + a[3], append_line % (d[0], d[1] + ' ' * (10 - len(d[1])), r2[0], r2[1])))
+        count += 1
+    banner.append('\x1b[38;5;255m════════╩═══════════╩════════════╩═══════╩══════╩══════════════════════════════════════╩ \x1b[38;5;78mTOP \x1b[38;5;79mNAME       \x1b[38;5;80mGOOD  \x1b[38;5;81mFAIL\x1b[0m')
+    banner.append('                   \x1b[38;5;255m( \x1b[38;5;70m🗸 \x1b[38;5;71mCURRENT NORMAL \x1b[38;5;196m✗ \x1b[38;5;197mCURRENT MAINTENENCE \x1b[38;5;255m)\n\x1b[38;5;226m♔  \x1b[38;5;227mMORE IS BETTER')
+    banner.append('\n\x1b[38;5;51mMADE \x1b[38;5;50mBY \x1b[38;5;202mt\x1b[38;5;203m.\x1b[38;5;204mm\x1b[38;5;205me\x1b[38;5;206m/\x1b[38;5;207mjack2044 \x1b[38;5;49mand \x1b[38;5;202mt\x1b[38;5;203m.\x1b[38;5;204mm\x1b[38;5;205me\x1b[38;5;206m/\x1b[38;5;207mIDKOTHERHEX1629')
+    return '\n'.join(banner) + '\x1b[0m'
+
+print(methods())
